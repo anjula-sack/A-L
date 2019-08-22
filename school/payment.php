6e178abff4d49123407e37349843dd63e0772aab
anjula44 <anjulashanaka@gmail.com>
2019-08-22 19:21:46 +0530

    success 1.0

--- school/payment.php
+++ school/payment.php
@@ -1,2 +1,22 @@
 <?php
+$account = $_POST['acc_no'];
+
+$conn = new mysqli('localhost', 'root', 'root123', 'payment');
+if ($conn->connect_error) {
+    die('Connection Error');
+}
+
+
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Payment Search</title>
+</head>
+<body>
+<h1>Your Payment History 2019</h1>
+
+</body>
+</html>
